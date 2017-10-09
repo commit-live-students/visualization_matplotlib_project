7f35d16bbf5438778dde52513150c9af403bc8bd
satishkhanna <snykhanna@gmail.com>
2017-10-09 07:45:50 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -4,5 +4,11 @@ import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
+def plot_deliveries_by_team():
+    grouped = ipl_df.groupby(['batting_team'])
+    df = grouped.agg({'delivery':'sum'}).reset_index()
+    x_len = len(df['batting_team'])
+    plt.bar(np.arange(0,x_len), df['delivery'])
+    plt.xticks(np.arange(0,x_len), df['batting_team'], rotation=45)
+    plt.show()
 # Solution
