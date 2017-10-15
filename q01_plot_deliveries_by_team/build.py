d2df8a115691bbe0d4b4e65e8429290a3fc6f757
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-10-15 12:17:43 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -4,5 +4,19 @@ import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_deliveries_by_team():
+        team_series = pd.Series(ipl_df["delivery"].values,ipl_df["batting_team"])
+        d1 = team_series.groupby(team_series.index).agg('count')
+        x = np.array(d1.index)
+        y = d1.values.astype(int)
 
-# Solution
+        ind = np.arange(len(x))
+
+        plt.bar(ind,y)
+        plt.title("total deliveries played by team")
+        plt.xlabel("batting teams")
+        plt.ylabel("total deliveries played")
+        plt.xticks(ind-1,x,rotation=45)
+        plt.show()
+
+plot_deliveries_by_team()
