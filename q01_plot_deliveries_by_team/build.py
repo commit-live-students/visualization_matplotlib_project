19a87453c29e6964ec7287961b037ae539417622
swapnilrt <swapnil13488@gmail.com>
2017-10-16 09:52:53 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,8 +1,16 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
-# Solution
+def plot_deliveries_by_team():
+    data = ipl_df['batting_team']
+    xy = data.groupby(ipl_df['batting_team']).count().sort_values(ascending=False)
+    x_label = range(1,14)
+    y_label = xy
+    plt.bar(x_label,y_label)
+    plt.xticks(x_label, xy.index, rotation=90)
+    plt.show()
+    return
