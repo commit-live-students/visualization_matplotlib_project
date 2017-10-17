4a9a16a7b29413cfc56e3bbfc6b0ee8e02bd7586
Sakshi-BUK <sakshi.dhokrat96@gmail.com>
2017-10-17 18:20:00 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,8 +1,20 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-ipl_df = pd.read_csv('../data/ipl_dataset.csv', index_col=None)
+ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_matches_by_team():
 
-# Solution
+    s1=ipl_df.groupby('batting_team')['match_code'].agg(['count'])
+    #print(s1)
+    plt.title('Total no of matches played')
+    plt.xlabel('team names')
+    plt.ylabel('count of matches')
+    plt.bar(np.arange(len(s1)),s1.values)
+    plt.xticks(np.arange(len(s1)),s1.index,rotation=45)
+
+    plt.show()
+
+plot_matches_by_team()
