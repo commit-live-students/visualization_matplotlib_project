c21d6ad25e15a7894d13751a8b91149e5f2583b4
jyotika03 <jyotika.tewari1992@yahoo.com>
2017-10-18 17:17:22 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,3 +1,4 @@
+# %load q03_plot_innings_runs_histogram/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,16 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_innings_runs_histogram():
+
+    fil1=ipl_df['inning']==1
+    fil2=ipl_df['inning']==2
+    x=ipl_df[fil1].groupby('match_code')['runs'].sum()
+    y=ipl_df[fil2].groupby('match_code')['runs'].sum()
+
+
+    fig, axes = plt.subplots(nrows=1,ncols=2)
+    axes[0].hist(x)
+    axes[1].hist(y)
+    plt.show()
+plot_innings_runs_histogram()
