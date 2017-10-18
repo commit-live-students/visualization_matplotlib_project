4fedf698bad069369f4d50cdaa4ac8d0fdb36977
saylijawale123 <saylijawale123@gmail.com>
2017-10-18 04:37:48 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -6,3 +6,10 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_innings_runs_histogram():
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    first = ipl_df[(ipl_df['inning'] == 1)]
+    second = ipl_df[(ipl_df['inning'] == 2)]
+    axes[0].hist(first['runs'], bins=10)
+    axes[1].hist(second['runs'],bins=10)
+    plt.show()
