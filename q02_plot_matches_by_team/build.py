4aa94018381736f407f8441868e593434b9c9bf6
Rahul0211 <rahuljain86@hotmail.com>
2017-10-22 11:35:44 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -5,4 +5,15 @@ import matplotlib.pyplot as plt
 ipl_df = pd.read_csv('../data/ipl_dataset.csv', index_col=None)
 
 
-# Solution
+def plot_matches_by_team():
+    team_match_counts = ipl_df.groupby(by='batting_team').agg('nunique').match_code
+    row_count = team_match_counts.shape[0]
+    x_series = np.arange(0, row_count, 1)
+    plt.figure(figsize=(20,5))
+    plt.bar(x_series, team_match_counts)
+    plt.xlabel('Batting Team')
+    plt.ylabel('# of matches')
+    plt.title('total number of matches played by each team')
+    plt.xticks(x_series, team_match_counts.index, rotation=45)
+    plt.show()
+plot_matches_by_team()
