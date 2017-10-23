15ab97304472188223ce3d2954ff9f59024d91a8
gaurav-barclays <gaurav.simply@gmail.com>
2017-10-23 04:08:46 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,8 +1,10 @@
+# %load q04_plot_runs_by_balls/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
+def plot_runs_by_balls():
+    return ()
 # Solution
