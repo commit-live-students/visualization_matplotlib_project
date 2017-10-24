9560f89258c57b6c7fa8e936d0fd7b2f74af5a06
sagard21 <sagard21@gmail.com>
2017-10-24 16:49:45 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -6,3 +6,13 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_innings_runs_histogram():
+    my_df = ipl_df.groupby(['inning','match_code']).sum()
+    fst_inn = my_df.loc[1]['total']
+    scn_inn = my_df.loc[2]['total']
+    fig, axes = plt.subplots(nrows = 1, ncols = 2)
+    axes[0].hist(fst_inn)
+    axes[0].set_title('1st Innings')
+    axes[1].hist(scn_inn)
+    axes[1].set_title('2nd Innings')
+    plt.show()
