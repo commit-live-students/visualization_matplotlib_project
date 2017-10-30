edb0061c5cb5666089630304ccbdbc126a3ff097
shashi07 <shashi.mishra43@gmail.com>
2017-10-30 02:00:41 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -4,5 +4,16 @@ import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
+def plot_innings_runs_histogram():
+    x = ipl_df.groupby(['match_code','inning'] , as_index=False).agg({'delivery':'count','runs':'sum'})
+    f, axarr = plt.subplots(2, sharex=True)
+    y1 = x['inning'] == 1
+    y2=x['inning'] == 2
+    y1 = x[y1]
+    y2=x[y2]
+    i1 = range(0,len(y1.index))
+    i2 = range(0,len(y2.index))
+    axarr[0].bar(i1,y1['runs'])
+    axarr[0].set_title('Sharing X axis')
+    axarr[1].bar(i2,y2['runs'])
 # Solution
