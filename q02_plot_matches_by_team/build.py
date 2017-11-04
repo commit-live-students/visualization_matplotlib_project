a29d023afa0073e968c616429761813be4ef7e05
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-11-04 18:13:53 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -4,5 +4,8 @@ import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('../data/ipl_dataset.csv', index_col=None)
 
-
-# Solution
+def plot_matches_by_team():
+    new_df = ipl_df.groupby(['match_code','batting_team'],as_index=False).count()[['batting_team','delivery']]
+    new_df.groupby('batting_team',as_index=False).sum()
+    delivery_df.plot(kind='bar')
+    plt.show()
--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.pyplot.switch_backend('agg')
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
+def plot_innings_runs_histogram():
+    first_inning = ipl_df[ipl_df['inning'] == 1]
+    second_inning = ipl_df[ipl_df['inning'] == 2]
+    plt.subplot(1,2,1)
+    plt.hist(first_inning.groupby('match_code',as_index=False).sum()['total'])
+    plt.subplot(1,2,2)
+    plt.hist(second_inning.groupby('match_code',as_index=False).sum()['total'])
+    plt.show()
 # Solution
