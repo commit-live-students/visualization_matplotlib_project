ca1185587cadbaf1f74f1accc15987d03b82f7aa
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-11-04 18:47:16 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.pyplot.switch_backend('agg')
 
 ipl_df = pd.read_csv('../data/ipl_dataset.csv', index_col=None)
 
-def plot_matches_by_team():
-    new_df = ipl_df.groupby(['match_code','batting_team'],as_index=False).count()[['batting_team','delivery']]
-    new_df.groupby('batting_team',as_index=False).sum()
-    delivery_df.plot(kind='bar')
+def plot_runs_by_balls():
+
+    ball_played_df = ipl_df.groupby(['match_code','batsman'],as_index=False).count()['delivery']
+    runs_scored_df = ipl_df.groupby(['match_code','batsman'],as_index=False).sum()['runs']
+    plt.scatter(ball_played_df,runs_scored_df)
     plt.show()
