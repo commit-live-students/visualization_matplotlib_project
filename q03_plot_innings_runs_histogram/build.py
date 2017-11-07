fd9d06c408142cba9a6ca346ee450ccb7ba59601
darshind <doshi.darshin@gmail.com>
2017-11-07 17:01:00 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
-ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+ipl_df = pd.read_csv('/home/darshind/Workspace/code/visualization_matplotlib_project/data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_innings_runs_histogram():
+
+    grouped = ipl_df.groupby('inning')
+    grouped_plot = (grouped.agg({
+    'delivery': 'count',
+    'total': 'sum'}))
+    grouped_plot.plot.bar()
+plot_innings_runs_histogram()
