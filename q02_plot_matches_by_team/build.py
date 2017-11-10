59b914a249ab4656f24e1e42e885e1711e32d90d
ABHISHEK THOMBRE <ug201314001@iitj.ac.in>
2017-11-10 13:05:01 +0530

    updates

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-ipl_df = pd.read_csv('../data/ipl_dataset.csv', index_col=None)
+ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
