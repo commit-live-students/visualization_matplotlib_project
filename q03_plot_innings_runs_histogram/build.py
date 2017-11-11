48cdc788623340f0a583771eff6846130746b0e8
jmcode79 <mukherjee.jeet@gmail.com>
2017-11-11 07:28:25 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,8 +1,19 @@
+# %load q03_plot_innings_runs_histogram/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+
+def plot_innings_runs_histogram():
+    Y = ipl_df.groupby(['match_code','inning','batsman']).agg({'delivery': pd.Series.sum, 'runs':pd.Series.sum })
+    fig,axes = plt.subplots(nrows=1, ncols=2)
+    Y.plot.hist()
+    plt.show()
+
+
+
+
 
 
 # Solution
