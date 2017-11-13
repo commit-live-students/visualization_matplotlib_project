99cf4049b1a3524cf7426c469397d5a6881e4770
PawanMathur <pawannupur@gmail.com>
2017-11-13 02:56:31 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -6,3 +6,17 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+
+    innings_runs =  ipl_df.groupby(['inning','batting_team'])['runs'].sum()
+    print innings_runs
+    print type(innings_runs)
+    print innings_runs.index
+    plt.figure(figsize=(10,6))
+    #plt.plot (by='inning', kind = 'bar', use_index=True, title = "Runs made in Innings")
+    plt.subplot(121)
+    innings_runs.loc[1,:].plot(kind = 'bar', use_index=True, title = "Runs made in 1st Inning")
+    plt.subplot(122)
+    innings_runs.loc[2,:].plot(kind = 'bar', use_index=True, title = "Runs made in 2nd Inning")
+    plt.show()
+
+#plot_innings_runs_histogram()
