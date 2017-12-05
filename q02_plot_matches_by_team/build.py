72e3c06ca16594737098861ab8396bec137f8da7
chiragpujari <chg.p786@gmail.com>
2017-12-05 16:20:12 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -6,3 +6,8 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    teams=ipl_df[['batting_team']]
+    g_teams=teams.groupby('batting_team')
+    g_teams[['batting_team']].count().plot(kind='bar')
+    plt.show()
