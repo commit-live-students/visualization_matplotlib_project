c24e3148a8bca0e2204d8e1b3b566954d86963fa
bhushan45 <bhushanshewale45@gmail.com>
2017-12-05 10:38:24 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -6,3 +6,9 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_deliveries_by_team():
+    df=ipl_df[['batting_team','delivery']]
+    counts=df.groupby('batting_team').count()
+    sort=counts.sort_values(by='delivery',ascending=False)
+    sort.plot(kind='bar')
+    plt.show()
