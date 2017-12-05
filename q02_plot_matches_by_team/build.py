080a017af18416058733602250bbf32f7086b9e6
honey079sam <honey079sam@gmail.com>
2017-12-05 13:10:08 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -6,3 +6,9 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    df=ipl_df[['match_code','batting_team']]
+    #new_df=df.pivot_table('match_code',index='batting_team',aggfunc='nunique')
+    new_df = df.groupby('batting_team').nunique()
+    new_df['match_code'].plot(kind='bar')
+    plt.show()
