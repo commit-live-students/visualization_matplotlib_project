e44f2e75c75ad42f1fccf4b521d2d5deacbf0f9d
vp999 <patilvish@gmail.com>
2017-12-06 00:40:04 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -6,3 +6,7 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    teams_in_each_match =ipl_df.groupby(['match_code','batting_team']).batting_team.nunique()
+    teams_in_each_match.groupby(['batting_team']).count().plot.bar()
+    plt.show()
