d672a407d7b1b3576284982ff0b13846d8dab558
Abhishek <abhisheksubu92@gmail.com>
2017-12-13 17:15:10 +0530

    updated test cases

--- q01_plot_deliveries_by_team/tests/test_plot_deliveries_by_team.py
+++ q01_plot_deliveries_by_team/tests/test_plot_deliveries_by_team.py
@@ -2,5 +2,5 @@ from unittest import TestCase
 
 
 class TestPlot_deliveries_by_team(TestCase):
-    def test_plot_deliveries_by_team(self):
+    def test_plot_deliveries_by_team_compare(self):
         self.assertTrue(True)
