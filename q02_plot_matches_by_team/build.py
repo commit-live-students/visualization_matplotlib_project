31745ce33899ff85f1b341f5b398fefdb337461e
ms10031990 <ms10031990@yahoo.com>
2017-12-15 16:12:38 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -5,4 +5,7 @@ import matplotlib.pyplot as plt
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
-# Solution
+def plot_matches_by_team():
+    counter=ipl_df.groupby("batting_team").agg({"match_code": pd.Series.nunique})
+    counter.plot(kind='bar')
+    plt.show()
