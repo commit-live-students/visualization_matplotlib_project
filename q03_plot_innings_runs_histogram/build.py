e8575712abe23966e3327e4fca288ad175d02e98
palash1512 <palash9515@gmail.com>
2017-12-17 11:12:55 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -6,3 +6,7 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+plot_innings_runs_histogram():
+    data = ipl_df.groupby(['match_code','inning'])
+    data['inning'].count().plot(kind='bar')
+    plt.show()
