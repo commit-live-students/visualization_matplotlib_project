3030df0a582c3e166c12c6bde1de069ffe4a7f5f
Jayaraj-bv <JAYARAJ.BV@gmail.com>
2017-12-18 15:39:01 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
-
-
 # Solution
+def plot_deliveries_by_team():
+    ipl_df1=ipl_df.pivot_table('batsman',aggfunc=np.size,index='team1')
+    ipl_df1.plot(kind='barh')
+    plt.show()
+    return
+
+#plot_deliveries_by_team()
