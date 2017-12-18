2c6dc3264fe55a740826bc4772d05e0f79ab663e
vaithyanathanramanathan <vaithy31@gmail.com>
2017-12-18 20:46:01 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from pandas import Series, DataFrame
+from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
-
-
-# Solution
+def plot_matches_by_team():
+	data1=ipl_df.groupby('batting_team').agg({'match_code': 'nunique'})
+	data1.plot(kind='bar')
+	plt.show()
