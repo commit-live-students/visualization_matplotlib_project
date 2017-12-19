9475f0e96fc7dfb59ccd9e896ea9f72b125293e5
aarthikat <aarthika.t@gmail.com>
2017-12-19 18:02:59 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -4,5 +4,9 @@ import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
+def plot_matches_by_team():
+    ipl_df1 = ipl_df.groupby('team1').match_code.nunique()
+    ipl_df1.plot(kind='bar')
+    plt.show()
+    return
 # Solution
