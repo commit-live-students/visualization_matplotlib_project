b862f1f6aa4ed45e6cd6759f3bfc58a53ad021c1
sjhonny03 <SJHONNY03@gmail.com>
2017-12-20 18:53:15 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -5,4 +5,9 @@ import matplotlib.pyplot as plt
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
-# Solution
+def plot_runs_by_balls():
+    delivery=pd.DataFrame({'delivery' : ipl_df.groupby(['match_code','batsman'])['delivery'].count()}).reset_index()
+    runs=pd.DataFrame({'runs' : ipl_df.groupby(['match_code','batsman'])['runs'].sum()}).reset_index()
+    merge=pd.merge(delivery, runs, how='inner', on=['match_code', 'batsman'])
+    plt.scatter(merge['delivery'], merge['runs'])
+    plt.show()
