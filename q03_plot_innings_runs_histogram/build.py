baacc4f72dd46241a24fb78b15d155f07ded4d2e
shaikferozgithub <shaikferozmail@gmail.com>
2017-12-22 08:05:32 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -6,3 +6,20 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+
+ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+
+def plot_innings_runs_histogram():
+    res1=ipl_df[ipl_df ['inning']==1]
+    res2=ipl_df[ipl_df ['inning']==2]
+
+    res_1=pd.DataFrame({'inning1' : res1.groupby(['match_code','inning'])['runs'].sum()}).reset_index()
+    res_2=pd.DataFrame({'inning2' : res2.groupby(['match_code','inning'])['runs'].sum()}).reset_index()
+    res_final=pd.merge(res_1, res_2, how='inner', on=['match_code'])
+    plt.figure(figsize=(10,4))
+    plt.subplot(121)
+    sns.distplot(res_final['inning1'], kde=False)
+    plt.subplot(122)
+    sns.distplot(res_final['inning2'], kde=False)
+    plt.tight_layout()
+    plt.show()
