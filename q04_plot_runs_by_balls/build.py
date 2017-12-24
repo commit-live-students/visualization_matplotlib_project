c9ca84d15279814d184a10f87a75527b3bfdf469
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2017-12-24 07:17:17 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,8 +1,19 @@
-import pandas as pd
+# %load q04_plot_runs_by_balls/build.py
+from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
 
-ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+def plot_runs_by_balls():
+    ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+    df1 = DataFrame(ipl_df[['batsman','delivery','match_code']].groupby(['batsman','match_code'])['delivery'].count())
+    df2 = DataFrame(ipl_df[['batsman','runs','match_code']].groupby(['batsman','match_code'])['runs'].sum())
+    merg_df=[df1,df2]
+    final_df = pd.concat(merg_df,axis=1)
+    one_bats = final_df.loc[['A Ashish Reddy'],:]
+    one_bats.plot.scatter('delivery','runs')
+    plt.show()
 
-
-# Solution
+plot_runs_by_balls()
