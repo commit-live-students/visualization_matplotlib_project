15da112007ccebf1f484e1e66824df19fa5d6021
nazirwa <nazirmail@gmail.com>
2017-12-24 12:07:12 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,8 +1,26 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+import matplotlib
+matplotlib.use('tkagg')
+import matplotlib.pyplot as plt
+
+def plot_innings_runs_histogram():
+    plt.close('all') # restting the canvas
+    plot_data = ipl_df[['batting_team','inning','runs']].groupby(['batting_team','inning']).agg({np.sum})
+    plot1 = plot_data.query('inning == 1')
+    plot2 = plot_data.query('inning == 2')
+    f, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True)
+    plot1.plot(kind='hist', ax=ax1)
+    plot2.plot(kind='hist', ax=ax2)
+    f.autofmt_xdate() # To rotate x axis labels
+    plt.show()
+
+    return None
+
+# plot_innings_runs_histogram()
