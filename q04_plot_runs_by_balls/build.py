b5d465c6eae75d1f129dc69e8243f7717eda62ed
nazirwa <nazirmail@gmail.com>
2017-12-24 12:53:23 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,8 +1,26 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+import matplotlib
+matplotlib.use('tkAgg')
+import matplotlib.pyplot as plt
+
+def plot_runs_by_balls():
+    plot_data = ipl_df[['batsman', 'delivery', 'runs']].groupby(['batsman']).agg(
+        {'delivery':np.count_nonzero, 'runs':np.sum})
+
+    plt.close('all')
+    f,ax = plt.subplots()
+    plot_data.plot(kind='scatter', x='delivery', y='runs', ax=ax)
+    f.suptitle('Balls vs Runs')
+    plt.xlabel = 'Balls played' # Not showing up
+    plt.ylabel = 'Runs Scored' # Not showing up
+    plt.show()
+    return None
+
+# plot_runs_by_balls()
