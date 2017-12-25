b2d428709bb2a4c8ad144adbe8a4ec13bb59a93a
iamsathishsatz <Sathishbabu.s.g@gmail.com>
2017-12-25 04:15:08 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -6,3 +6,14 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    ipl_df1 = ipl_df[['batting_team','match_code']]
+
+
+    aggregation = {'match_code': 'nunique'}
+    uniquecount = ipl_df1.groupby('batting_team').agg(aggregation).reset_index()
+
+    # print uniquecount
+    plt.bar(uniquecount.index, uniquecount['match_code'])
+    plt.xticks(uniquecount.index, uniquecount['batting_team'], rotation=90)
+    plt.show()
