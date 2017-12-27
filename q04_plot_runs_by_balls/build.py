da1dbf8eb6ec2f345aff77be3480b510fbc43874
swapnilpatil22 <swapnil.patil2205@hotmail.com>
2017-12-27 12:46:56 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -6,3 +6,9 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_runs_by_balls():
+    tablea=ipl_df.pivot_table(values='runs',aggfunc='sum',index=['batsman'] ,columns='match_code' )
+    tableb=ipl_df.pivot_table(values='delivery',aggfunc='count',index=['batsman'] , columns='match_code')
+    tablec=pd.merge(tablea,tableb)
+    tablec.plot(kind='scatter')
+    plt.show()
