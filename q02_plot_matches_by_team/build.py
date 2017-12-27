027f24ddb5ef754d2393d8e60018ac62fc1f7836
swapnilpatil22 <swapnil.patil2205@hotmail.com>
2017-12-27 12:42:12 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -6,3 +6,7 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    action_count=ipl_df.pivot_table('match_code',aggfunc='nunique',index='batting_team')
+    action_count.plot(kind='bar')
+    plt.show()
