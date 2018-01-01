c08e62590b18cd4c0a7c1a668d1f309eda4de041
harshavachhani <harshavachhani@gmail.com>
2018-01-01 12:01:03 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -6,3 +6,10 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_deliveries_by_team():
+    data = ipl_df.pivot_table(values='delivery',index=['batting_team'],aggfunc='count')
+    data.plot(kind='bar')
+    plt.title('Number of Deliveries Faced by IPL Teams')
+    plt.xlabel('IPL Teams')
+    plt.ylabel('Number of Deliveries')
+    plt.show()
