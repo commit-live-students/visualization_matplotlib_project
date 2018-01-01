6c1c24374721f46bc8f588ab8021ed210dae3081
NeelkanthMehta <neilstrong2003@yahoo.com>
2018-01-01 18:27:26 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -8,3 +8,17 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 # Solution
 def plot_innings_runs_histogram():
+    # Creating a desired data into dataframe
+    df00 = pd.pivot_table(ipl_df,values='runs', index=['batting_team'],columns='inning',aggfunc=np.sum)
+
+    # Creating plot
+    # The reason for creating a bar plot as opposed to histogram is that the question required us to assess
+    # the score of each team; which generates very few data instances (<30); and it's easy to eyeball over the bar charts
+    plt.figure()
+    plt.subplot(2,1,1)
+    df00[1].plot(kind='bar')
+    plt.show()
+    plt.subplot(2,1,2)
+    df00[2].plot(kind='bar')
+    plt.show()
+    return None
