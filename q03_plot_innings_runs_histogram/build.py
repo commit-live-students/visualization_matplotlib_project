5f5daa4ab1cc4ac8572bfff09d623d893d6db266
NeelkanthMehta <neilstrong2003@yahoo.com>
2018-01-01 16:50:14 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_innings_runs_histogram():
--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -6,3 +6,9 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_runs_by_balls():
+    x = ipl_df.groupby('batsman')['delivery'].sum()
+    y = ipl_df.groupby('batsman')['runs'].sum()
+    plt.scatter(x=x, y=y)
+    plt.show()
+    return None
