ce77a9b62739a2bb281f53e5a66c408b10aa9c13
pasugithub <pasu.rm@gmail.com>
2018-01-03 16:32:34 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -4,5 +4,9 @@ import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
 # Solution
+def plot_matches_by_team():
+    umatches = ipl_df[['batting_team','match_code']].groupby('batting_team').agg({'match_code':pd.Series.nunique})
+    umatches.plot(kind='bar')
+    #plt.show()
+    return None
