9fec17fca6293c45faa3b7c47d68d0e27b63edea
gupta1006 <guptagauttam@gmail.com>
2018-01-06 10:18:50 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,8 +1,13 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
-
-
-# Solution
+def plot_matches_by_team():
+    a=ipl_df.groupby(by='batting_team').nunique()
+    plot=a['match_code'].plot(kind='bar')
+    plot.set_ylabel("Match_Count")
+    plot.set_xlabel("Teams")
+    plt.show()
+plot_deliveries_by_team()
