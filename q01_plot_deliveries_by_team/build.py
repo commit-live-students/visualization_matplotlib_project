6370c95e86be1fbbedcde223a733a3ac28009c29
Vijay1306 <vijaymadurai@yahoo.com>
2018-01-07 14:09:33 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -2,7 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+def plot_deliveries_by_team():
+    ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+    num_of_deliveries = ipl_df.groupby('batting_team')['delivery'].sum()
+    num_of_deliveries.plot()
+    plt.xticks(rotation=45)
+    plt.show()
+
 
 
 # Solution
