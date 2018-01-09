954c6b51e0d75a621c1ea188e9bface24cb7e569
Rajachellan <raja.june15@gmail.com>
2018-01-09 04:48:50 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,8 +1,22 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+#import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+import numpy as np
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from pandas import Series, DataFrame
+
+from greyatomlib.pandas_project.q01_read_csv_data_to_df.build import read_csv_data_to_df
+
+ipl_df = read_csv_data_to_df("data/ipl_dataset.csv")
+
+def get_unique_venues():
+    return pd.unique(ipl_df['venue'])
