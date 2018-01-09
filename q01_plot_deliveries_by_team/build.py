34f9d4dcda19ca8584146befed6aa37d6786b0dc
Rajachellan <raja.june15@gmail.com>
2018-01-09 04:46:49 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -6,3 +6,11 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+path = "data/ipl_dataset.csv"
+
+path1 = "data/ipl_dataset.csv"
+
+path2 = "data/weather_2012.csv"
+
+def read_csv_data_to_df(path):
+    return pd.read_csv(path)
