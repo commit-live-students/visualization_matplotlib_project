5914b6f95acb3716059bc94f7dd25e5bffdbaba6
gupta1006 <guptagauttam@gmail.com>
2018-01-15 12:13:48 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -4,5 +4,9 @@ import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_runs_by_balls():
+    table = ipl_df.pivot_table(index ='delivery', values=['runs'], aggfunc = np.sum)
+    plt.scatter(table.index,table['runs'])
+    plt.show()
 
 # Solution
