d0c8bdcefd2b37870a11ace73b0d908f6ef1bc8a
viishnu712 <viishnu712@gmail.com>
2018-02-03 17:23:20 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -6,3 +6,14 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+
+def plot_deliveries_by_team():
+    ipl_dict = {}
+    for i in (ipl_df['batting_team'].value_counts()).index:
+        freq =  ((ipl_df['batting_team']==i).value_counts())
+        actual_delivery = freq.get(True)
+        ipl_dict[i]=actual_delivery
+    teams = list(ipl_dict.keys())
+    deliveries = list(ipl_dict.values())
+    plt.bar(range(len(ipl_dict)),deliveries,tick_label=teams)
+    plt.show()
