703e81a7ecfa1ef01483eec209ac1fb6ddb97047
Srishtisawla <sawlasrishti@gmail.com>
2018-02-06 19:22:34 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -3,6 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
-
-
+def plot_innings_run_histogram():
+    a = ipl_df[['match_code','inning','delivery']]
+    b = ipl_df[['match_code','inning','runs']]
+    c = a.groupby(as_index=False,by=('match_code','inning')).count()
+    d = b.groupby(as_index=False,by=('match_code','inning')).sum()
+    e = c.merge(d)
+    inning1 = e[e['inning']==1]
+    inning2 = e[e['inning']==2]
+    inning1[['match_code','delivery','run']].plot(kind='hist')
+    plt.show()
+    inning2[['match_code','delivery','run']].plot(kind='hist')
+    plt.show()
 # Solution
