c49c3d006737155079b0a62fadc1808e677463bd
sohelahmadkhan <sohelahmadkhan000@gmail.com>
2018-02-13 18:03:29 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -8,7 +8,7 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 # Solution
 def plot_innings_runs_histogram():
 
-    a = ipl_df.pivot_table('runs',aggfunc = 'count',index = 'match_code',columns = 'inning')
+    a = ipl_df.pivot_table('runs',aggfunc = np.sum,index = 'match_code',columns = 'inning')
     a.plot(kind='hist')
     plt.show()
 
