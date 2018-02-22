06d3b8496c4887a06893e90dbf0ab9f5f170a556
seema200 <singh.seema200@gmail.com>
2018-02-22 08:17:00 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas import DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,3 +7,14 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_deliveries_by_team():
+    teams=np.unique(ipl_df['batting_team'])
+    ipl_df['count']=1
+    deliveries=ipl_df.pivot_table(index='batting_team',aggfunc=np.sum )
+    deliveries_count=deliveries['count']
+
+    plt.xlabel('Teams')
+    plt.ylabel('Deliveries Played ')
+    plt.bar(teams,deliveries_count)
+    plt.xticks(teams,rotation='vertical')
+    plt.show()
