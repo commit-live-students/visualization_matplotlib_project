77ddb69f1472c7b83d33641ee6d1cbba80cc7ace
CharulataShelar <charulatashelar@gmail.com>
2018-02-24 18:24:36 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -6,3 +6,13 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_deliveries_by_team():
+    #pd.pivot_table(data=ipl_df.loc[:,['match_code', 'runs']], index='match_code', columns=ipl_df.loc[:,'runs'], aggfunc='count')
+    #each match--> each team--> total deliveries
+    df = ipl_df.groupby('batting_team')
+    team_total_delivery = df['delivery'].sum()
+    #print team_total_delivery
+    team_total_delivery.plot( kind='bar')
+    plt.show()
+
+plot_deliveries_by_team()
