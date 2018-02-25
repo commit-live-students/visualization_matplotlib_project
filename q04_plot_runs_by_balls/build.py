80e0d946b32039e38114f692d7a0eca48b2bc872
HerambD <heramb.dharmadhikari@gmail.com>
2018-02-25 17:34:57 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -6,3 +6,14 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_runs_by_balls():
+    c = ipl_df.groupby(['match_code','batsman'])['match_code'].count()
+    d = ipl_df.groupby(['match_code','batsman'])['runs'].sum()
+    a1 = pd.DataFrame(c)
+    b1 = pd.DataFrame(d)
+    a1 =  a1.join(b1)
+    plt.scatter(a1['match_code'],a1['runs'])
+    plt.title("ball vs runs per match/innings")
+    plt.xlabel("balls")
+    plt.ylabel("runs")
+    plt.show()
