7cb68d8c64250bf3a8a81e7968cafeeb4197cc6f
kvin15 <vinayakkhamkar@gmail.com>
2018-02-25 18:14:41 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -6,3 +6,18 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_runs_by_balls():
+    # Get the delivery count per batsman
+    df_del_count = ipl_df[['batsman','delivery','runs']]\
+                    .groupby(['batsman'])[['delivery']].count()
+    # Get the total runs scored by batsman
+    df_runs_total = ipl_df[['batsman','delivery','runs']]\
+                    .groupby(['batsman'])[['runs']].sum()
+
+    # Merge the DataFrame  (index as batsman)
+    df_merged = pd.merge(df_del_count, df_runs_total,left_index=True, right_index=True)
+    ax = df_merged.plot(kind='scatter',x='delivery',y='runs',grid=True)
+    ax.set_title("Total Runs Scored against Number of Deliveries")
+    ax.set_xlabel("Number of Deliveries")
+    ax.set_ylabel("Total Runs Scored")
+    plt.show()
