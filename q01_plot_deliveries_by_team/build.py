442cb6fefd376a70e47f40d9d765b748d39d2c10
sagarrawale <sagar.rawale3@gmail.com>
2018-02-27 10:10:08 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -4,5 +4,12 @@ import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_deliveries_by_team():
+    x_axis = np.arange(len(ipl_df['batting_team']))
+    y_axis = ipl_df['delivery']
+    plt.bar(x_axis,y_axis)
+    plt.show()
 
+
+plot_deliveries_by_team()
 # Solution
