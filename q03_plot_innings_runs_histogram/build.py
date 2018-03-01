022dfddb69aa55184d053c5e9b9879f6f7ccc5d6
Sharad30 <sharad30.ss@gmail.com>
2018-03-01 15:03:54 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,14 +1,26 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from pandas import DataFrame
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 def plot_innings_runs_histogram():
 
-    s = ipl_df.groupby(['inning','total'])
-    print s['total'].sum()
+    s = DataFrame(ipl_df['total'].groupby([ipl_df['match_code'], ipl_df['batsman']]).sum())
+    s1 = DataFrame(ipl_df['delivery'].groupby([ipl_df['match_code'], ipl_df['batsman']]).count())
+    #s3 = pd.DataFrame( np.hstack((s1.values, s2.values))
+    #plt.hist(s)
+    #plt.show()
+
+    runs_scored = pd.pivot_table(ipl_df, values='total', index=['match_code', 'batsman','inning'], \
+                      aggfunc=np.sum)
+    balls_played = pd.pivot_table(ipl_df, values='delivery', index=['match_code', 'batsman','inning'], \
+                      aggfunc= len)
+    data = pd.concat(runs_scored, balls_played)
+    print data
+    #plt.hist(data['delivery'])
     #plt.hist()
 
 plot_innings_runs_histogram()
--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -4,5 +4,12 @@ import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_runs_by_balls():
 
-# Solution
+    runs_scored = pd.pivot_table(ipl_df, values='total', index=['match_code', 'batsman','inning'], \
+                      aggfunc=np.sum)
+    balls_played = pd.pivot_table(ipl_df, values='delivery', index=['match_code', 'batsman','inning'], \
+                      aggfunc= len)
+
+    plt.scatter(runs_scored,balls_played)
+    plt.show()
