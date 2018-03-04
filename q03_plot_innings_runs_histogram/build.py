162f6c2cbde5b8b7836b3fa48f85cb7d6dac91d9
sonal7930 <sonal.chandra7930@gmail.com>
2018-03-04 16:05:56 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,32 +1,30 @@
+# %load q03_plot_innings_runs_histogram/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+
 def plot_innings_runs_histogram():
-    #runs per match
-    count_runs = ipl_df[['inning','match_code','runs']]
-    pivot_count_runs=count_runs.pivot_table('runs',aggfunc=np.sum,index='match_code',columns='inning').fillna(0)
-    left=pivot_count_runs
+    group_sum = ipl_df.groupby(['match_code', 'inning']).sum().reset_index()
+    
+    inning1_df = group_sum[group_sum['inning'] == 1]
+    print inning1_df.head(1)
+    inning2_df = group_sum[group_sum['inning'] == 2]
+    print inning2_df.head(1)
 
-    #Count of bowls played per bowler
-    ipl_df['frequency']=1
-    count_balls=ipl_df[['inning','bowler','frequency']]
-    pivot_count_balls=count_balls.pivot_table('frequency',aggfunc=np.sum,index='bowler',columns='inning').fillna(0)
-    right=pivot_count_balls
+    fig, axes = plt.subplots(1, 2)
+    
+    axes[0].hist(inning1_df['runs'])
+    axes[1].hist(inning2_df['runs'])
+    plt.show();
 
-    #Merging two dataframes to get runs per inning
-    merge_df=pd.merge(left,right,how='outer')
-    print merge_df.head()
-
-    plt.hist(merge_df,bins=250)
-    plt.subplots(1,2)
-    xaxes=['1','2']
-    yaxes=['runs']
-    plt.title('Runs per innings')
-    plt.xlabel('Matches')
-    plt.ylabel('Runs Scored')
-
-    plt.show()
 plot_innings_runs_histogram()
+
+
+
+
+    
+
+
