e0f5f84080e3ecceba85cb52f4962502d1d56f6b
abkedar <anupbhande@gmail.com>
2018-03-07 07:26:02 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from pandas import DataFrame, Series
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
 # Solution
+def plot_innings_runs_histogram():
+    df = DataFrame(ipl_df[['inning', 'runs', 'batting_team']].groupby(['batting_team', 'inning'])['runs'].sum())
+    table = df.pivot_table(index='batting_team', columns='inning', values ='runs')
+    table1 = table.sort_index()
+    table1.plot(kind='bar', stacked = True)
+    plt.show()
--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from pandas import DataFrame, Series
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_runs_by_balls():
+    ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+    df1 = DataFrame(ipl_df[['batsman', 'delivery', 'match_code']].groupby(['batsman', 'match_code'])['delivery'].count())
+    df2 = DataFrame(ipl_df[['batsman', 'runs', 'match_code']].groupby(['batsman', 'match_code'])['runs'].sum())
+    merge_df=[df1, df2]
+    final_df=[df1, df2]
+    final_df = pd.concat(merge_df, axis=1)
+    one_bats = final_df.loc[['A Ashish Reddy'], :]
+    one_bats.plot.scatter('delivery', 'runs')
+    plt.show()
 
-# Solution
+#print plot_runs_by_balls()
