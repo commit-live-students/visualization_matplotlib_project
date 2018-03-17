770c81dc70c4b7dc54e4a2f879cc50bfd3f6542f
sejal-varu <sejalvaru@gmail.com>
2018-03-17 15:39:05 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -6,3 +6,7 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    pd_df = ipl_df.groupby("batting_team")["match_code"].nunique()
+    pd_df.plot(kind="bar")
+    plt.show()
