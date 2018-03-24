dd60c64fbf5ceb4a2f297a4757114abb36e32a3d
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-24 05:33:22 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,20 +1,24 @@
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-ipl = pd.read_csv('data/ipl_dataset.csv', index_col=0)
-
-
+ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 def plot_innings_runs_histogram():
-    group_sum = ipl_df.groupby(['match_code', 'inning']).sum().reset_index()
-    inning1_df = group_sum[group_sum['inning'] == 1]
-    inning2_df = group_sum[group_sum['inning'] == 2]
+    total_runs = ipl_df.groupby(['inning','match_code']).agg({'runs':['sum']}).reset_index()
+    first_inning_data = total_runs[total_runs['inning'] == 1]
+    second_inning_data = total_runs[total_runs['inning'] == 2]
+    fig = plt.figure()
+    fig.add_subplot(121)
+    plt.hist((first_inning_data.iloc[:,first_inning_data.columns.get_level_values(1)=='sum']).values)
+    plt.title('IPL 1st inning')
+    plt.xlabel('Runs')
+    plt.ylabel('Frequency')
+    fig.add_subplot(122)
+    plt.hist((second_inning_data.iloc[:,second_inning_data.columns.get_level_values(1)=='sum']).values)
+    plt.title('IPL 2nd inning')
+    plt.xlabel('Runs')
+    plt.ylabel('Frequency')
+    plt.show()
 
-    fig, axes = plt.subplots(1, 2)
-    
-    axes[0].hist(inning1_df['runs'])
-    axes[1].hist(inning2_df['runs'])
 
-plot_innings_runs_histogram()
 
