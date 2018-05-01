7f8f111aefd8494fa3eb1bf0f614fa9e12548306
rushi0109 <rushi0109@gmail.com>
2018-05-01 09:10:04 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,3 +1,4 @@
+# %load q03_plot_innings_runs_histogram/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,25 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_innings_runs_histogram():
+    iplGroupBy = ipl_df.groupby(['match_code','inning']).sum()
+    fig, ax = plt.subplots(nrows=1, ncols=2)
+    
+    #return ipl_df.columns.values
+    inningInfo = iplGroupBy.loc[:,'total'].reset_index()
+    firstInningBool = inningInfo['inning'] == 1
+    secondInningBool = inningInfo['inning'] == 2
+    data = list()
+    data.append(inningInfo[firstInningBool]['total'])
+    data.append(inningInfo[secondInningBool]['total'])
+    ax = ax.ravel()
+    for idx,ax1 in enumerate(ax):
+        maxData = data[idx].max()
+        print(maxData)
+        bins=np.arange(0,maxData,20)
+        ax1.hist(data[idx],bins=bins,linewidth=0.2,edgecolor='black')
+        
+    fig.show()
+    #return inningInfo[firstInningBool]['total']
+plot_innings_runs_histogram()
+
