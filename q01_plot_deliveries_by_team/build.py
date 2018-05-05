54e115f1a2928b0c965a7f41f8b06f8090d059dc
chirag-shinde <shinde.chirag2204@gmail.com>
2018-05-05 00:51:28 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,11 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_deliveries_by_team():
+    team_data = ipl_df.pivot_table(index='batting_team',aggfunc='count')['delivery']
+    plt.bar(team_data.index,team_data)
+    plt.plot()
+    plt.xticks(rotation='90')
+    plt.show()
+plot_deliveries_by_team()
+
