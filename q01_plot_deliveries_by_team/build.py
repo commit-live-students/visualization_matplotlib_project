12e92acd22c2a0b3e7574aae4fe3c65b43e08fed
asdoc <asdocode@gmail.com>
2018-05-06 11:47:16 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,8 +1,23 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt; plt.rcdefaults()
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
-# Solution
+def plot_deliveries_by_team():
+    ser = ipl_df.groupby(['batting_team']).count()['match_code']
+    y_pos = np.arange(len(ser))
+    plt.bar(y_pos, ser, align='center', alpha=0.5)
+    x_pos = ser.index
+    plt.xticks(y_pos, x_pos, rotation=90)
+    plt.ylabel('Deliveries')
+    plt.title('Teams Vs Deliveries')
+    plt.show()
+
+
+
+
+
+
