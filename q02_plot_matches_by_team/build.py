38e9f7b1c2d858da34afee69e97156e6bfa12ced
bharadwajk9 <utkb.k9@gmail.com>
2018-05-24 15:11:58 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,12 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    team_data = ipl_df.groupby('batting_team')['match_code'].nunique()
+    plt.bar(team_data.index, team_data)
+    plt.xticks(rotation = 90)
+    plt.plot()
+    plt.show()
+
+plot_matches_by_team()
+
