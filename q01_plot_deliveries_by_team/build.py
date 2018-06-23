80fefdcc67b3f6c237aa90f9893a2eab036da7e3
vnylp <vinitdhamale.we@gmail.com>
2018-06-23 16:08:27 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,8 +1,14 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_deliveries_by_team():
+    ipl_df['team1'].value_counts().plot(kind='bar')
+    plt.show()
 
-# Solution
+plot_deliveries_by_team()
+
+
