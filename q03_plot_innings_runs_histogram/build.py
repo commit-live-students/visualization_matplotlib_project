6aafc2d9d3e83c2443ba9eaf740604b8bc3641de
gauravpednekar <grv.pednekar@gmail.com>
2018-06-23 14:52:21 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,3 +1,4 @@
+# %load q03_plot_innings_runs_histogram/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,12 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_innings_runs_histogram():
+    df_s=ipl_df.groupby(['match_code','batsman']).sum()
+    df_c=ipl_df.groupby(['match_code','batsman']).count()
+    plt.scatter(df_c['delivery'],df_s['runs'])
+    plt.show()
+
+plot_innings_runs_histogram()
+
+
