e240c881248199a13ce2945cf2d7cb7b43e4349d
Tusharsharma86 <tusharsharma86@gmail.com>
2018-07-29 19:00:17 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,4 +6,49 @@ plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
-# Solution
+# Function Create
+def plot_matches_by_team():
+    '''Function to plot matches for each team'''
+    #dataframe for only teams and matches
+    match=ipl_df[['batting_team','match_code']]
+    #using unique value function for team and match combination 
+    yaxis = match.groupby(['batting_team']).nunique()['match_code']
+    #x axis to get indiex which is teams
+    xaxis = yaxis.index
+
+    #plot bar chart with labels
+    plt.bar(xaxis,yaxis, label = '# Matches')
+    plt.xticks(rotation = '90')
+    plt.legend()
+    plt.show()
+    
+#Function call 
+plot_matches_by_team()
+
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+
+# Function Create
+def plot_matches_by_team():
+    '''Function to plot matches for each team'''
+    #dataframe for only teams and matches
+    match=ipl_df[['batting_team','match_code']]
+    #using unique value function for team and match combination 
+    yaxis = match.groupby(['batting_team']).nunique()['match_code']
+    #x axis to get indiex which is teams
+    xaxis = yaxis.index
+
+    #plot bar chart
+    plt.bar(xaxis,yaxis, label = '# Matches')
+    plt.xticks(rotation = '90')
+    plt.legend()
+    plt.show()
+    
+#Function call 
+plot_matches_by_team()
+
+
