9670a09ad3e5841580d22a0e48331a5727742161
Ranjandass <dasranjan444@gmail.com>
2018-07-31 12:52:08 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,8 +1,16 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
 # Solution
+def plot_matches_by_team():
+    filter_team = ipl_df.groupby(['batting_team']).agg('nunique')
+    xseries = filter_team.index
+    yseries = filter_team['match_code']
+    plt.plot(xseries,yseries)
+    plt.show()
+    
+
