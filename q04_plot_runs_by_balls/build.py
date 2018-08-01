0842aefee7642173a608f70b7ee023f2e1f3e7e5
Ranjandass <dasranjan444@gmail.com>
2018-08-01 06:21:30 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,3 +1,4 @@
+# %load q04_plot_runs_by_balls/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,11 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_runs_by_balls():
+    runs_df = ipl_df.groupby(['match_code','batsman']).agg(np.sum)['runs']
+    delivery_df = ipl_df.groupby(['match_code','batsman']).agg('count')['delivery']
+    plt.scatter(runs_df, delivery_df, c='b')
+    plt.show()
+   
+
+
