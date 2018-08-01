cb57161158c0c1b1337a24729fdadbdb88102243
rianboy18 <pranavr95.pn@gmail.com>
2018-08-01 12:31:29 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,8 +1,15 @@
+# %load q04_plot_runs_by_balls/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+df=pd.DataFrame(ipl_df)
+r=df.groupby('match_code').agg(np.sum)
+a=pd.DataFrame(r['delivery'])
+b=pd.DataFrame(r['runs'])
+plt.scatter(a,b)
 
-# Solution
+
+
