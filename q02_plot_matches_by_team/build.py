e0cd633bb44ec4f21923b3591a0a5771919c116e
sonalverma1 <sonalverma1@gmail.com>
2018-08-02 18:38:16 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,8 +1,22 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.switch_backend('agg')
-ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_matches_by_team():
+    ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+    ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+    df = ipl_df[['batting_team','match_code']].groupby('batting_team').agg('count')
+    plt.plot(df.index,df.values)
+    plt.show()
 
 # Solution
+plot_matches_by_team()
+
+
+
+
+
+
+
+
