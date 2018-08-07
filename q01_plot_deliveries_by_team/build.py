e00f34beac9284cfec130cb7b1b063a7bf0a107f
ankit-a-mehta <ankitmehta195@gmail.com>
2018-08-07 17:27:17 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,3 +8,15 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_deliveries_by_team():
+    #Approach 1 - Using matplotlib
+    %matplotlib inline
+    ipl_df['batting_team'].value_counts().plot.bar()
+    plt.show()
+    #Approach 2
+    #batting_count = ipl_df['batting_team']
+    #plt.barh(batting_count)
+    #plt.show()
+    
+plot_deliveries_by_team()
+
