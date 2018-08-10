9e593f91b8e8208101696c2fef6fe6e27e9fdadd
ankit-a-mehta <ankitmehta195@gmail.com>
2018-08-10 05:10:28 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,13 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    %matplotlib inline
+    batting_team_code = ipl_df[['batting_team','match_code']]
+    batting_team_code = batting_team_code.groupby(['batting_team'])['match_code'].nunique().plot(kind='bar', title='Team v/s Match Count')
+    
+    #plt.bar(batting_team_code)
+    #plt.show()
+    
+plot_matches_by_team()
+
