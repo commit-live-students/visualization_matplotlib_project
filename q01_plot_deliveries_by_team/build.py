72a244c98e4231e1404d25a30e1ce3b748d9aecd
sonik8494 <sourabh.niks1994@gmail.com>
2018-08-10 05:50:53 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,9 +1,18 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
-
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_deliveries_by_team():
+    a =ipl_df['batting_team'].value_counts()
+    v =list(a.index)
+    plt.bar(v,a)
+    plt.show()
+
+
+
+
