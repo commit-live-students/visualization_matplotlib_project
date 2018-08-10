4b0d4b1b0f859b372f66f3fc680440e636d3ba01
raahuul <er.rahul.v27@gmail.com>
2018-08-10 06:26:31 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,3 +1,4 @@
+# %load q04_plot_runs_by_balls/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,11 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_runs_by_balls():
+    df1=ipl_df.groupby(['match_code','batsman']).agg('count')['delivery'].to_frame('delivery')
+    df2=ipl_df.groupby(['match_code','batsman']).agg(sum)['runs'].to_frame('runs')
+    result = pd.concat([df1, df2], axis=1)
+    result.plot(x='delivery', y='runs', style='o')
+
+
+
