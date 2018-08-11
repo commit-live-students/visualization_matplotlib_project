081692b7fb811dea7b46cde0d9a099b20de9fc49
ramyakrishnan86 <ramya_krishnan86@yahoo.co.in>
2018-08-11 18:54:56 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,9 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    bar_plot = ipl_df.groupby(['batting_team'])['match_code'].agg('nunique').plot.bar()
+    bar_plot.set_xlabel('team names')
+    bar_plot.set_ylabel('unique/distinct count of matches')
+    return
+
