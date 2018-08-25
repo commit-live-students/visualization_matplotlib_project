448f549bc237a8be17ad5e7b091ac06658c22ccd
y0ge5h <c.9.yogesh@gmail.com>
2018-08-25 08:24:03 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,9 +1,13 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
-
+def 
 
 # Solution
+
+
+
