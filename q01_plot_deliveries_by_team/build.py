b7646fe6a713ab06794dc3fc166c5dff33e30e12
psicktrick <poojarysaurabh932@gmail.com>
2018-09-22 11:14:23 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,3 +8,17 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+# def plot_deliveries_by_team():
+#     x = np.arange(len(list(set(list(ipl_df['batting_team'])))))
+    
+#     plt.bar(x, )
+    
+# plot_deliveries_by_team()    
+
+# list(ipl_df['batting_team'])
+# x = np.arange(len(list(set(list(ipl_df['batting_team'])))))
+def plot_deliveries_by_team():
+    ipl_df['batting_team'].value_counts().plot(kind='bar')
+
+
+
