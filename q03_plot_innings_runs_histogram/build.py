f0f6774e7159c8674eeaa47abe10e56718ba63be
aksmungekar <mungekarakshay@yahoo.com>
2018-09-22 14:02:41 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,8 +1,13 @@
+# %load q03_plot_innings_runs_histogram/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_innings_runs_histogram():
+    df=ipl_df[['inning','runs']]
+    df.groupby('inning')['inning'].sum().plot(kind='hist')
+    plt.show()
 
-# Solution
+
