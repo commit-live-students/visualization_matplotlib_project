303f3a960dc013b1ee47c5a9abb72b4c8259c8f4
sannidh <soniknitr@gmail.com>
2018-09-22 14:54:25 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,3 +8,13 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_deliveries_by_team():
+    count_delivery=ipl_df.groupby(['team1']).agg('count')['match_code']
+    count_delivery.plot(kind='bar')
+    plt.show();
+plot_deliveries_by_team()
+
+
+
+
+
