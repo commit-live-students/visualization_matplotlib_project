253460a7ef66ba02c535ed764df61475faa57f1b
psicktrick <poojarysaurabh932@gmail.com>
2018-09-23 09:10:43 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,4 +6,19 @@ plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
+
 # Solution
+def plot_matches_by_team():
+    df = ipl_df[['batting_team', 'match_code']].drop_duplicates()
+    df.set_index('match_code', inplace=True)
+
+
+    df['batting_team'].value_counts().plot(kind='bar')
+    
+plot_matches_by_team()
+
+
+
+
+
+
