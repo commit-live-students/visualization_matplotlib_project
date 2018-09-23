b9dd8a9ed03318e4a49d5bf36377b85b09f26aaa
aksmungekar <mungekarakshay@yahoo.com>
2018-09-23 17:19:51 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -7,11 +7,10 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 def plot_runs_by_balls():
     df=ipl_df[['inning','runs']]
-    df1=ipl_df['inning'].value_counts()
-    df2=df.groupby('inning')['inning'].sum()
-    x=df1.tolist()
-    y=df2.tolist()
-    plt.scatter(x, y,alpha=0.5)
+    df1=pd.DataFrame(ipl_df['inning'].value_counts())
+    df2=pd.DataFrame(ipl_df.groupby(['inning'])['runs'].sum())
+    runs_vs_balls=df1.join(df2)
+    plt.scatter(runs_vs_balls['inning'],runs_vs_balls['runs'])
     plt.show()
 
 
