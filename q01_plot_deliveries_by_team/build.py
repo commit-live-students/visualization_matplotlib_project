8a367030c12e507cb2d42861d123f938ac72a0db
tracedence <shoaibrashid887@gmail.com>
2018-09-25 11:27:10 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -10,9 +10,16 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 # Solution
 def plot_deliveries_by_team():
     
-    fig = plt.figure()
+    #create figure for drawing data
+    fig = plt.figure() 
+    
+    # group data by batting team and use count as aggregator, then select delivery column since result in the form of pandas
     data = ipl_df.groupby('batting_team').count()['delivery']
-    plt.plot(data.index, data.values)
+    
+    # plot bar graph
+    plt.bar(data.index, data.values)
+    
+    # rotate ticks of x axis by 30
     plt.xticks(rotation = 30)
     plt.show()
     
