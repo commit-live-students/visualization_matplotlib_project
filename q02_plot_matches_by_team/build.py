f92086ce563aee9cdb2059e98658f9c5c8699fd2
KamalShillongwala <qamaldas@gmail.com>
2018-09-25 06:50:10 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,8 +1,19 @@
+# %load q02_plot_matches_by_team/build.py
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_matches_by_team():
+    x = sorted(ipl_df['batting_team'].unique())
+    y = ipl_df.groupby('batting_team')['batting_team'].nunique()
+    plt.bar(x,y,0.35)
+    plt.xticks(x, rotation=90)
+    plt.show()
+
+
+
+
