8980d989daf7f0f19e31a8e6d8c30d17f7ddd025
sunilhariharan <sunilhariharan@gmail.com>
2018-09-27 09:28:24 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,5 +6,29 @@ plt.switch_backend('agg')
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_deliveries_by_team():
+    
+    c=ipl_df.groupby('batting_team').agg({'delivery':'count'}).reset_index()
+    c.plot(kind='bar')
+    plt.xticks(index,list(c['batting_team']))
+    plt.xlabel('Batting team')
+    plt.ylabel('Deliveries')
+    plt.show()
 
-# Solution
+
+c=ipl_df.groupby('batting_team').agg({'delivery':'count'}).reset_index()
+c['delivery']
+type(c)
+c.iloc[:,0]
+
+c=c.reset_index()
+c.iloc[:,0]
+ax=c[['batting_team','delivery']].plot(kind='bar')
+plt.show()
+ax
+ax=c[['batting_team','delivery']].plot(kind='bar')
+ax
+plt.show()
+ax
+
+
