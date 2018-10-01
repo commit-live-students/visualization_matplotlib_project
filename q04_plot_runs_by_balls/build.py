a48c14eac50da3707d7fdf1d6e4179dc7b501c3e
preetiail <ailpreeti@ymail.com>
2018-10-01 03:25:02 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,8 +1,23 @@
+# %load q04_plot_runs_by_balls/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.switch_backend('agg')
+#plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+m=ipl_df.groupby('match_code')['bowler'].nunique()
+n=ipl_df.groupby('match_code')['bowler','runs'].agg({'runs':'sum'})
+plt.scatter(m,n)
+
+plt.show()
 
 # Solution
+#def plot_runs_by_balls():#h=ipl_df['bowler'].value_counts.agg('sum')
+
+
+
+
+
+
+
+
