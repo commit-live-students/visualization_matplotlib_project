d69e3c16cd77ceceb614101350453380e9acb348
akshaylakade <Aakshaylakade@gmail.com>
2018-10-16 07:35:12 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,3 +1,4 @@
+# %load q04_plot_runs_by_balls/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,10 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_runs_by_balls():
+    x = ipl_df['delivery']
+    y = ipl_df['runs']
+    plt.scatter(x,y)
+    plt.show
+
+
