0adccfc66737a5dc6300289944ad3764eda2eba7
akshaylakade <Aakshaylakade@gmail.com>
2018-10-16 03:36:00 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,11 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+x = ipl_df['batting_team'].value_counts()
+y = ipl_df['winner'].unique()
+
+plt.bar(x,y)
+
+
+
+
