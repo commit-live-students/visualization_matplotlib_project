355e2f115aff1cd9758e98c0dbc0713dab2bb6c2
akashhchatterjee <akashh.chatterjee@gmail.com>
2018-10-19 18:21:18 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,3 +1,4 @@
+# %load q04_plot_runs_by_balls/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,12 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_runs_by_balls():
+    ipl_df1 = ipl_df[['batsman','delivery']].groupby(['batsman']).count()
+    ipl_df2 = ipl_df[['batsman','runs']].groupby(['batsman']).sum()
+    ipl_data=ipl_df1.join(ipl_df2, how='inner')
+    plt.scatter(ipl_df1, ipl_df2)
+    plt.show()
+    
+plot_runs_by_balls()
+
