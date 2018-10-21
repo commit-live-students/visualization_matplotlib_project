f0d01612b1fe1e632b65f2d8af9f535f497a2cd4
avnishxlr8 <avnish.vish95@gmail.com>
2018-10-21 11:13:57 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,8 +1,20 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
+def plot_matches_by_team():
+    x_team = ipl_df['batting_team']
+    #y_count = ipl_df.groupby('batting_team')['match_code'].count()
+    y_count = ipl_df['match_code']
+    #print(y_count)
+    plt.scatter(x_team,y_count)
+    plt.plot(x_team,y_count)
+    plt.show()
 # Solution
+plot_matches_by_team()
+
+
+
