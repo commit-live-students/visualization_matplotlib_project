3a83c9a3517d3be3f29c41b873b18d16cccf8a19
avnishxlr8 <avnish.vish95@gmail.com>
2018-10-21 11:54:17 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,8 +1,23 @@
+# %load q03_plot_innings_runs_histogram/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
 # Solution
+
+def plot_innings_runs_histogram():
+    #x = ipl_df.where(ipl_df['inning']==1)['runs']
+    x1 = ipl_df.where(ipl_df['inning']==1, other = 0)['runs']
+    x2 = ipl_df.where(ipl_df['inning']==2, other = 0)['runs']
+    #print(x)
+    plt.figure()
+    plt.subplot(121)
+    plt.hist(x1)
+    plt.subplot(122)
+    plt.hist(x2)
+
+plot_innings_runs_histogram()
+
+
