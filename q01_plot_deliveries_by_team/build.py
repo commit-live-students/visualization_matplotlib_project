8fe9896f01630147d83194cd934c1ffef026704f
avnishxlr8 <avnish.vish95@gmail.com>
2018-10-21 10:36:57 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,5 +6,19 @@ plt.switch_backend('agg')
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_deliveries_by_team():
+    x = ipl_df['batting_team']
+    #y = ipl_df.groupby('batting_team')['delivery'].count()
+    y = ipl_df['delivery']
+    print(x.shape,y.shape)
+    #plt.scatter(x,y)
+    plt.plot(x,y)
+    plt.show()
+
+#plot_deliveries_by_team()
+
 
 # Solution
+
+
+
