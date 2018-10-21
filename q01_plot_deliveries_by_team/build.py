be8243b99d8681a3e9df58d53cce7b884aacbb62
amrutasawant-greyatom <amrutasawantk@gmail.com>
2018-10-21 11:14:32 +0000

    Done

--- q01_plot_deliveries_by_team/build.py
+++ q01_plot_deliveries_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q01_plot_deliveries_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,5 +6,17 @@ plt.switch_backend('agg')
 
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
-
 # Solution
+def plot_deliveries_by_team():
+    
+    newData = ipl_df.groupby(['batting_team']).agg('count')
+    x = newData.index.values
+    y = newData['delivery'].values
+    plt.barh(x, y)
+    plt.show()
+    
+plot_deliveries_by_team()
+
+
+
+
