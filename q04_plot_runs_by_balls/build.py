02da07feda25d57da0345d90fe9629886e1fe855
Ajpalav <ajit.palav@gmail.com>
2018-11-29 06:25:03 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,8 +1,29 @@
+#%load q04_plot_runs_by_balls/build.py
+
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-plt.switch_backend('agg')
-ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
+from matplotlib import pyplot as plt
+
+ifl_df=pd.read_csv('./data/ipl_dataset.csv')
+# ifl_df.columns
+# runs,delivery,batsman
+
+def plot_runs_by_balls():
+
+    gr=ifl_df.groupby('batsman')
+    balls=pd.DataFrame(gr['delivery'].count())
+    runs=pd.DataFrame(gr['runs'].sum())
+
+    A=balls.merge(runs, left_index=True,right_index=True, how='inner')
+
+    plt.scatter(A.runs,A.delivery)
+    plt.xlabel=('runs')
+    plt.ylabel=('deliveries')
+
+    plt.show()
+
+# runs
+
+# plt.scatter(ifl_df[)
 
 
-# Solution
