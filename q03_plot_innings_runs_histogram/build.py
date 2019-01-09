920aab37e613638db9a41156d1d96ac109f9ce77
Sandesh373 <dyavanapellisandesh373@gmail.com>
2019-01-09 09:29:07 +0000

    Done

--- q03_plot_innings_runs_histogram/build.py
+++ q03_plot_innings_runs_histogram/build.py
@@ -1,8 +1,30 @@
+# %load q03_plot_innings_runs_histogram/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
+def plot_innings_runs_histogram():
+    ipl_df1=ipl_df.groupby(['inning','match_code'], as_index=False)['runs'].sum()
+    fig,axs=plt.subplots(1,2)
+    axs[0].hist(ipl_df1[ipl_df1.inning==1].runs)
+    axs[0].set_title('First Inning runs distribution')
+    axs[1].hist(ipl_df1[ipl_df1.inning==2].runs)
+    axs[1].set_title('Second Inning runs distribution')
+    plt.show()
+    fig
+fig
+ipl_df1=ipl_df.groupby(['inning','match_code'], as_index=False)['runs'].sum()
+ipl_df1
+ipl_df1[ipl_df1.inning==1].runs
+ipl_df1[ipl_df1.inning==2].runs
+fig,axs=plt.subplots(1,2)
+axs[0].hist(ipl_df1[ipl_df1.inning==1].runs)
+axs[0].set_title('First Inning runs distribution')
+axs[1].hist(ipl_df1[ipl_df1.inning==2].runs)
+axs[1].set_title('Second Inning runs distribution')
+plt.show()
+fig
 
-# Solution
+
