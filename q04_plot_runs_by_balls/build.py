cc38ec60bc72fa3368041808416dd29fe90d760f
iampa1 <iampa1gehaney@gmail.com>
2019-01-14 12:53:27 +0000

    Done

--- q04_plot_runs_by_balls/build.py
+++ q04_plot_runs_by_balls/build.py
@@ -1,3 +1,4 @@
+# %load q04_plot_runs_by_balls/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,17 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+def plot_runs_by_balls():
+    plt.scatter(ipl_df['runs'],ipl_df.index)
+    plt.show()
+    
+
+plt.scatter(ipl_df['runs'],ipl_df.index)
+plt.show()
+fig, ax = plt.subplots(1,2)
+ax[0].hist(x, bins=10, alpha = 0.5, color = 'r')
+ax[1].hist(y, bins=10, alpha = 0.5, color = 'g')
+plt.show()
+plot_runs_by_balls()
+
+
