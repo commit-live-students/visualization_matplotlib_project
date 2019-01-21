c85103051c40e33858fd6a477952fc8ab3c483a1
sagark93 <sagarkulkarni093@gmail.com>
2019-01-21 11:32:09 +0000

    Done

--- q02_plot_matches_by_team/build.py
+++ q02_plot_matches_by_team/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_matches_by_team/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,3 +7,21 @@ ipl_df = pd.read_csv('data/ipl_dataset.csv', index_col=None)
 
 
 # Solution
+
+def plot_matches_by_team():
+    batting_team = ipl_df['batting_team']
+    matches_count = batting_team.value_counts()
+    x_series = np.arange(len(matches_count.index))
+    plt.bar(x_series, matches_count)
+    plt.xticks(x_series, matches_count.index.values, rotation=90)
+    plt.show()
+
+
+plot_matches_by_team()
+
+
+
+
+
+
+
